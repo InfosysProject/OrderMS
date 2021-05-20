f81cc3b9e1f412947f942946b4caeea96bd3a69c
PreranaGhare <84308402+PreranaGhare@users.noreply.github.com>
2021-05-20 15:53:07 +0530

    Update OrderMsApplicationTests.java

--- src/test/java/com/infosys/ecart/OrderMS/OrderMsApplicationTests.java
+++ src/test/java/com/infosys/ecart/OrderMS/OrderMsApplicationTests.java
@@ -1,13 +1,13 @@
-package com.infosys.ecart.OrderMS;
+// package com.infosys.ecart.OrderMS;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class OrderMsApplicationTests {
+// @SpringBootTest
+// class OrderMsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
